316e6291f24c40a5d2c50380dc8c32f0fe306893
PG-54 <gedonisticussr@gmail.com>
2025-08-14 17:52:36 +0300

    feat: add all character-name-input-page music and animation logic

--- script.js
+++ script.js
@@ -1,7 +1,30 @@
 const charCreateAudio = document.querySelector('.char-creation-bg-music');
-charCreateAudio.volume = .1
-const bgMusicPlay = () => charCreateAudio.play()
-window.addEventListener('click', bgMusicPlay);
+charCreateAudio.volume = .1;
+const bgMusicPlay = () => charCreateAudio.play();
+const interactionEvents = [
+    'click',
+    'keydown',
+    'mousemove',
+    'scroll',
+    'touchstart',
+    'touchend',
+    'wheel'
+];
+interactionEvents.forEach(interaction => {
+    window.addEventListener(interaction, bgMusicPlay);
+})
 const van = document.querySelector('.van-sound');
-van.volume = .2
-van.addEventListener('click', () => van.play())
+van.volume = .1;
+van.addEventListener('click', () => van.play());
+
+document.querySelector('.char-creation button').addEventListener('click', () => {
+    if (!document.querySelector('.char-creation input').value) return
+    interactionEvents.forEach(interaction => {
+        window.removeEventListener(interaction, bgMusicPlay)
+    })
+    setTimeout(() => {       
+        document.querySelector('.char-creation').style.top = '100%';
+        document.querySelector('.char-creation-submit').play();
+        charCreateAudio.pause();
+    })
+})
