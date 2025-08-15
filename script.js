0cd259adbfb6038a4b9c6d41fdfde5d06a004947
PG-54 <gedonisticussr@gmail.com>
2025-08-15 19:51:09 +0300

    feat: stylize basic character-page

--- script.js
+++ script.js
@@ -18,13 +18,20 @@ van.volume = .1;
 van.addEventListener('click', () => van.play());
 
 document.querySelector('.char-creation button').addEventListener('click', () => {
-    if (!document.querySelector('.char-creation input').value) return
+    if (!document.querySelector('.char-creation input').value) return;
+    if (document.querySelector('.char-creation input').value.length > 30) {
+        alert('Имя не может быть длиннее 30 символов!');
+        return
+    }
     interactionEvents.forEach(interaction => {
         window.removeEventListener(interaction, bgMusicPlay)
     })
-    setTimeout(() => {       
+    setTimeout(() => {
+        document.querySelector('.CHARACTER').style.display = 'flex';       
         document.querySelector('.char-creation').style.top = '100%';
         document.querySelector('.char-creation-submit').play();
         charCreateAudio.pause();
-    })
+        document.querySelectorAll('.char-name').forEach(field => field.innerHTML = document.querySelector('.char-creation input').value)
+    }, 10)
+    setTimeout(() => document.querySelector('.char-creation').style.display = 'none', 3000)
 })
